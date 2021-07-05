56a478ac4e6ce3eb534b7b4e7fd6cc0b126fce7b
Dmitry Kankalovich <konkolovich.dmitry@gmail.com>
2021-07-05 10:23:47 +0200

    Added the actual task

--- src/test/java/com/interview/topdigits/AppShould.java
+++ src/test/java/com/interview/topdigits/AppShould.java
@@ -4,12 +4,28 @@ import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.MatcherAssert.assertThat;
 
+import java.util.List;
 import org.junit.jupiter.api.Test;
 
 public class AppShould {
 
+  /**
+   * Given the list of sequences of digits - See `input` var example.
+   *
+   * You must find the top N digits by it's frequency of appearance in the list.
+   *
+   * See below `expected` var for the example of output.
+   *
+   * */
   @Test
-  void add_two_numbers() {
-    assertThat(1 + 1, is(equalTo(2)));
+  void find_top_N_digits_by_frequency() {
+
+    final var input = List.of(1,1,1,2,2,3,3,3,3,3,4,4,4,4);
+    final var topN = 2;
+    final var expected = List.of(3, 4);
+
+    var result = new Solution().topDigits(input, topN);
+
+    assertThat(result, is(equalTo(expected)));
   }
 }
